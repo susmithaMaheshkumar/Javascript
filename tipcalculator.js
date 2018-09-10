4b6195c550824516d0f044c28adc1ad7ed5d5beb
susmithaMaheshkumar <42698400+susmithaMaheshkumar@users.noreply.github.com>
2018-09-10 09:04:27 -0400

    Learning Function calling

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,7 +1,7 @@
 /* Coding challenge of tip calculation*/
 
 
-bill =[199, 20, 168];
+/*bill =[199, 20, 168];
 tipcalculator (bill);
 
 function tipcalculator(bill){
@@ -16,7 +16,36 @@ function tipcalculator(bill){
 	}
 
 }
-
+  
 console.log(tipcalculator(bill[0])+bill[0]);
 console.log(tipcalculator(bill[1])+bill[1]);
 console.log(tipcalculator(bill[2])+bill[2]);
+*/
+/*function randomvalue(){
+	var score = Math.round(Math.random() * 10 ) + 1;
+	console.log(score);
+}
+
+randomvalue();*/
+
+function interviewQuestion(job) {
+	{
+		return function(name){
+			if (job === "designer"){
+			console.log(name+ "years to retirement");
+		}
+		}
+	}else if (job === "teacher"){
+		return function(name){
+			console.log( name + "what subject do you tech");
+		}
+	}else {
+		return function (name){
+			console.log( name + "what do you do");
+		}
+	}
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+teacherQuestion("jane");
+interviewQuestion ("baseball")("uber");
